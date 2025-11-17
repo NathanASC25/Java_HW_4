901b57659bfd31c6a1661f61fa0c821a650f6a97
NathanASC25 <javanathanjchin@gmail.com>
2025-11-17 01:18:39 -0500

    seventh commit

--- RandomChars.java
+++ RandomChars.java
@@ -6,6 +6,8 @@ public class RandomChars {
     }
     public static void generateNums() {
         char letter = 65;
+	// Extra variable totalChars to keep count of characters
+	int totalChars = 0;
         int upperCaseLetters[] = new int[26];
         for (int i = 0; i < 1000; i++) {
             int randomNum = (int) (Math.random() * 26) + 65;
@@ -14,6 +16,10 @@ public class RandomChars {
         for (int i = 0; i < upperCaseLetters.length; i++){
             System.out.printf("\n%c : %d", letter, upperCaseLetters[i]);
             letter += 1;
+	    // Extra Line to accumulate number of Characters.
+	    totalChars += upperCaseLetters[i];
         }
+	// Prints numbers of characters
+	System.out.printf("\n\nTotal Characters: %d\n", totalChars);
     }
 }
--- Workbook.java
+++ Workbook.java
@@ -1,5 +1,34 @@
 public class Workbook {
     public static void main(String[] args) {
-        
+        Store bigStore = new Store();
+	Store littleStore = new Store();
+
+	bigStore.name = "SaveAll";
+	bigStore.address = "1.23 E. 23rd";
+	bigStore.sqFeet = 9894;
+
+	littleStore.name = "Mom's Food";
+	littleStore.address = "83 N. Oak";
+	littleStore.sqFeet = 1213;
+
+	System.out.println("Store: " + bigStore.name);
+	System.out.println("at: " + bigStore.address);
+	System.out.println("has: " + bigStore.sqFeet + " feet.");
+
+	System.out.println("Store: " + littleStore.name);
+	System.out.println("at: " + littleStore.address);
+	System.out.println("has: " + littleStore.sqFeet + " feet.");
     }
+    /* Output: 
+     *
+     *
+     * Store: SaveAll
+     * at: 1.23 E. 23rd
+     * has: 9894 feet.
+     * Store: Mom's Food
+     * at: 83 N. Oak
+     * has: 1213 feet.
+     *
+     *
+     * */
 }
