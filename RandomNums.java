fe01c01ecfbf859b07b842c3772f2b9b2ad57726
NathanASC25 <javanathanjchin@gmail.com>
2025-11-17 00:50:02 -0500

    fifth commit

--- RandomNums.java
+++ RandomNums.java
@@ -1,19 +1,19 @@
 public class RandomNums {
     public static void main(String[] args) {
         System.out.print("\n");
-	generateNums();
-	System.out.print("\n");
+        generateNums();
+        System.out.print("\n");
     }
     public static void generateNums() {
-	char letter = 65;
+        char letter = 65;
         int upperCaseLetters[] = new int[26];
-	for (int i = 0; i < 1000; i++) {
-	    int randomNum = (int) (Math.random() * 26) + 65;
-	    upperCaseLetters[randomNum - 65] += 1;
-	}
-	for (int i = 0; i < upperCaseLetters.length; i++){
-	    System.out.printf("\n%c : %d", letter, upperCaseLetters[i]);
-	    letter += 1;
-	}
+        for (int i = 0; i < 1000; i++) {
+            int randomNum = (int) (Math.random() * 26) + 65;
+            upperCaseLetters[randomNum - 65] += 1;
+        }
+        for (int i = 0; i < upperCaseLetters.length; i++){
+            System.out.printf("\n%c : %d", letter, upperCaseLetters[i]);
+            letter += 1;
+        }
     }
 }
