e93024a448014b705a0ea9c3f4cb9d8b76e5af47
NathanASC25 <javanathanjchin@gmail.com>
2025-11-18 06:54:25 -0500

    fourteenth commit

--- Question2.java
+++ Question2.java
@@ -5,81 +5,81 @@ public class Question2 {
     public static void main(String[] args) throws Exception {
         // Test Case
         // Existing test file location
-	File testFile = new File("./TextFiles", "Test1.txt");
-	File output = new File("./TextFiles/output.txt");
-	File output2 = new File("./TextFiles/output2.txt");
-	String[] repNames = new String[100];
+        File testFile = new File("./TextFiles", "Test1.txt");
+        File output = new File("./TextFiles/output.txt");
+        File output2 = new File("./TextFiles/output2.txt");
+        String[] repNames = new String[100];
         double[] avgTripLengths = new double[100];
         int totalTestRecords = numRecords(repNames, avgTripLengths);
         compareTripLengths(output2, avgTripLengths, totalTestRecords);
         sortIndividuals(repNames, avgTripLengths, totalTestRecords);
-	PrintWriter writeOutput = new PrintWriter(output);
-	for (int i = 0; i < 100; i++) {
+        PrintWriter writeOutput = new PrintWriter(output);
+        for (int i = 0; i < 100; i++) {
             writeOutput.printf("Rep Name: %s \nAverage Trip Length: %.2f\n\n", repNames[i], avgTripLengths[i]);
-	}
-	writeOutput.close();
+        }
+        writeOutput.close();
     }
     // Part 1. 
     public static int numRecords(String[] names, double[] avgTripLengths) throws Exception {
         File testFile = new File("./TextFiles", "Test1.txt");
-	Scanner readFile = new Scanner(testFile);
+        Scanner readFile = new Scanner(testFile);
         int recordCount = 0;
         while (readFile.hasNext()) {
             String name = readFile.next();
-	    float firstTrip = readFile.nextFloat();
-	    float secondTrip = readFile.nextFloat();
-	    double avgMiles = (firstTrip + secondTrip) / 2;
-	    names[recordCount] = name;
-	    avgTripLengths[recordCount] = avgMiles;
-	    recordCount += 1;
+            float firstTrip = readFile.nextFloat();
+            float secondTrip = readFile.nextFloat();
+            double avgMiles = (firstTrip + secondTrip) / 2;
+            names[recordCount] = name;
+            avgTripLengths[recordCount] = avgMiles;
+            recordCount += 1;
         }
-	readFile.close();
-	return recordCount;
+        readFile.close();
+        return recordCount;
     }
     // Part 2.
     public static void compareTripLengths(File output, double[] avgTripLengths, int totalRecords) throws Exception {
-	PrintWriter writeOutput = new PrintWriter(output);
+        PrintWriter writeOutput = new PrintWriter(output);
         double totalTripLength = 0.0;
-	double overallAvgTripLength = 0.0;
-	int belowAvgLength = 0;
-	int equalAvgLength = 0;
-	int aboveAvgLength = 0;
+        double overallAvgTripLength = 0.0;
+        int belowAvgLength = 0;
+        int equalAvgLength = 0;
+        int aboveAvgLength = 0;
         for (int i = 0; i < avgTripLengths.length; i++) {
             totalTripLength += avgTripLengths[i];
         }
-	overallAvgTripLength = totalTripLength / totalRecords;
+        overallAvgTripLength = totalTripLength / totalRecords;
         for (int i = 0; i < avgTripLengths.length; i++) {
             if (avgTripLengths[i] > overallAvgTripLength) {
                 aboveAvgLength += 1;
-	    }
-	    else if (avgTripLengths[i] < overallAvgTripLength) {
+            }
+            else if (avgTripLengths[i] < overallAvgTripLength) {
                 belowAvgLength += 1;
 	    }
 	    else if (avgTripLengths[i] == overallAvgTripLength) {
                 equalAvgLength += 1;
 	    }
-	}
-	writeOutput.printf("Number of average trip lengths above overall average: %d\n", aboveAvgLength);
-	writeOutput.printf("Number of average trip lengths below overall average: %d\n", belowAvgLength);
-	writeOutput.printf("Number of average trip lengths equal to overall average: %d\n", equalAvgLength);
-	writeOutput.close();
+        }
+        writeOutput.printf("Number of average trip lengths above overall average: %d\n", aboveAvgLength);
+        writeOutput.printf("Number of average trip lengths below overall average: %d\n", belowAvgLength);
+        writeOutput.printf("Number of average trip lengths equal to overall average: %d\n", equalAvgLength);
+        writeOutput.close();
     }
-    // Part 3.
+       // Part 3.
     public static void sortIndividuals(String[] names, double[] avgTripLengths, int totalRecords) {
 	// If the first character of the second name belongs later in the alphabet compared to the first name, places are switched
         for (int i = 0; i < totalRecords - 1; i++) {
             char[] firstName = names[i].toCharArray();
-	    for (int j = i + 1; j < totalRecords; j++) {
+            for (int j = i + 1; j < totalRecords; j++) {
                 char[] secondName = names[j].toCharArray();
-		if (secondName[0] > firstName[0]) {
+	        if (secondName[0] > firstName[0]) {
                     String newFirstName = names[j];
-		    names[j] = names[i];
-		    names[i] = newFirstName;
+	            names[j] = names[i];
+	            names[i] = newFirstName;
                     double newFirstTripLength = avgTripLengths[j];
-		    avgTripLengths[j] = avgTripLengths[i];
-		    avgTripLengths[i] = newFirstTripLength;
-		}
+	            avgTripLengths[j] = avgTripLengths[i];
+	            avgTripLengths[i] = newFirstTripLength;
+	        }
 	    }
-	}
+        }
     }
 }
